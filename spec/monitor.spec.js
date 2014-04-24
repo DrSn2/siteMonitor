1fd06a4ac2c581975b6725685260143c9572233a
Jeremy Stowell <jeremy.stowell@gmail.com>
2014-04-24 16:28:05 -0600

    added monitor spec for testing in the future.

---
+++ spec/monitor.spec.js
@@ -0,0 +1,9 @@
+//var monitor = require('../monitor');
+
+describe("The Monitor", function() {
+    it("should contains spec with an expectation", function() {
+//        expect(true).toBe(true);
+//        console.log('monitor: ' + monitor.buyViaCounter);
+//        monitor.buyViaCounter == 0;
+    });
+});
