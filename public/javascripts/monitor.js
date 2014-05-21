6ffc7673ccadb1bf1dc7db5fe915bb63b5107f41
Jeremy Stowell <jeremy.stowell@gmail.com>
2014-05-21 17:00:48 -0600

    Added jquery to project.  Used query to toggle adding monitor section.

---
+++ public/javascripts/monitor.js
@@ -0,0 +1,9 @@
+$(document).ready( function () {
+    $("#toggleMonitor").click(function () {
+//        alert("Blah");
+    $("#monitorForm").toggle("slow", function () {
+        // Animation complete.
+    });
+    });
+});
+
