ca82bc9aaa2d09ed192174175b9ae00587abbe5e
Jeremy Stowell <jeremy.stowell@gmail.com>
2014-05-25 16:14:36 -0600

    changing booleans to strings.  This allows the heroku variable to work.

--- config/secrets.js
+++ config/secrets.js
@@ -25,9 +25,9 @@ module.exports = {
 
     serverURL: process.env.SERVER_URL || 'http://localhost:3000',
 
-    isMonitorActive: process.env.IS_MONITOR_ACTIVE.toBoolean || true,
+    isMonitorActive: process.env.IS_MONITOR_ACTIVE || 'true',
 
-    allowSiteSignup: process.env.ALLOW_SIGNUP.toBoolean || true,
+    allowSiteSignup: process.env.ALLOW_SIGNUP || 'true',
 
     mandrill: {
         login: process.env.MANDRILL_USERNAME || '',
